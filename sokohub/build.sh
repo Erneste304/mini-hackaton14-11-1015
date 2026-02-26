1a85cf39a4e764c61f212aeb6867eef5ce70d328
Erneste304 <ernestegisubizo83@gmail.com>
2026-02-26 18:34:35 +0200

    fix: add fix_render_db.py to build.sh hook to bypass shell restriction

--- sokohub/build.sh
+++ sokohub/build.sh
@@ -5,4 +5,8 @@ set -o errexit
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
+
+# Automatically fix inconsistent migration history on Render if it occurs
+python fix_render_db.py
+
 python manage.py migrate
